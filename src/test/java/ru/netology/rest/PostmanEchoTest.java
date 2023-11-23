dad8ab7a314c76988251a4bde3720c35b35bd33c
Tanya <Tanja_33@mail.ru>
2023-11-23 21:41:13 +0700

    installation failed

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(200)
+                .statusCode(300)
                 .body("data", equalTo("Hello"));
     }
 }
